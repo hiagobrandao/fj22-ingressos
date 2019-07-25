8e46dc7f0b176ab52beee7c17473a92f1543f456
hiago <hi.ago.14@hotmail.com>
2019-07-25 11:38:59 -0300

    alteraçoes de lugares e carrinho

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,43 +3,70 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Sessao {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
-	
+
 	@ManyToOne
 	private Sala sala;
 	@ManyToOne
 	private Filme filme;
-	
+
 	private BigDecimal preco;
-	
-	public Sessao() {
-		
+
+	@OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
+	private Set<Ingresso> ingressos = new HashSet<>();
+
+	public Set<Ingresso> getIngressos() {
+		return ingressos;
 	}
-	
+
+	public void setIngressos(Set<Ingresso> ingressos) {
+		this.ingressos = ingressos;
+	}
+
+	public Sessao() {
+
+	}
+
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.setHorario(horario);
 		this.setFilme(filme);
 		this.setSala(sala);
 		this.preco = sala.getPreco().add(filme.getPreco());
-		
+
 	}
 	
+	public boolean isDisponivel(Lugar lugarSelecionado) {
+		return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
+		
+	}
+
+	public Map<String, List<Lugar>> getMapaDeLugares() {
+		return sala.getMapaDeLugares();
+	}
+
 	public BigDecimal getPreco() {
 		return preco.setScale(2, RoundingMode.HALF_UP);
 	}
+
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
